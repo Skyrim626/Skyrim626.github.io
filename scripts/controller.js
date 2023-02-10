4db592701579e7f1db8e22a28472d757b18db838
Skyrim626 <sanchezhanszin@gmail.com>
2023-02-10 11:30:57 +0800

    added hero section

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,8 +1,8 @@
-var cursor = document.querySelector(".cursor");
+/* var cursor = document.querySelector(".cursor");
 var cursor2 = document.querySelector(".cursor2");
 document.addEventListener("mousemove", function(e) {
     cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
-});
+}); */
 
 /*Navigation Bar Scroll Event */
 window.addEventListener('scroll', function(){
